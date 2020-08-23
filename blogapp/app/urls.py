c21dabcf38265dbb071e215355c646c6c8ecf4bc
raizengxd <rickdavidsangalang@gmail.com>
2020-08-23 23:12:19 +0800

    add

--- blogapp/app/urls.py
+++ blogapp/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('x/', views.index, name='index'),
     path('ydf/a', views.myName, name='myNamxe'),
+    path('test', views.show_blog_post, name='myNamxe'),
 ]
--- blogapp/app/views.py
+++ blogapp/app/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import BlogPost
 def index(request):
     return HttpResponse("Hello, World!")
 def myName(request):
+
     return HttpResponse("testing xxD")
+def show_blog_post(request):
+    obj = BlogPost.objects.get(id=1)
+    context= {
+        'blog':obj
+    }
+    
+    return render(request,"blogpost.html",context)
