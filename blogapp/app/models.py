daed4bd65fa550ccb72980c9d2f0b76c7ce902d4
raizengxd <rickdavidsangalang@gmail.com>
2020-08-23 11:34:22 +0800

    adding models

--- blogapp/app/models.py
+++ blogapp/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    blogTitle= models.CharField()
+    blogContent = models.TextField()
+    blogDatePost = models.DateTimeField()
+
+    
+    
+
