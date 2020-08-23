08e406d58a821e4a95990b9e8f0a38dee2292306
raizengxd <rickdavidsangalang@gmail.com>
2020-08-23 13:46:43 +0800

    created database

--- blogapp/app/models.py
+++ blogapp/app/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
-    blogTitle= models.CharField()
+    blogTitle= models.CharField(max_length=300)
     blogContent = models.TextField()
     blogDatePost = models.DateTimeField()
 
-    
+    def __str__(self):
+        return self.blogTitle
+
+
+
     
 
