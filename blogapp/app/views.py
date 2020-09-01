03d164b7ddacacd4acc08786bc86d2b807c9f3a0
raizengxd <rickdavidsangalang@gmail.com>
2020-09-01 23:03:16 +0800

    add

--- blogapp/app/views.py
+++ blogapp/app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import BlogPost
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request,"index/index.html")
 def myName(request):
 
     return HttpResponse("testing xxD")
